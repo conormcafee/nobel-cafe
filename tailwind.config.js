7992feaaf39c1de6680900a3076cb6b0883727c3
Conor McAfee <hello@conormcafee.co.uk>
2020-01-28 21:43:10 +0000

    Moved gatsby to root

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   theme: {
-    extend: {},
+    fontFamily: {
+      heading: [`"Lato", sans-serif`],
+      base: [`"Alegreya", serif`],
+    },
   },
   variants: {},
   plugins: [],
