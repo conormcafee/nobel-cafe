f84394915e77eba5796ce2730d9509554a5ebc58
Conor McAfee <hello@conormcafee.co.uk>
2020-10-16 16:38:28 +0100

    Quick and rough tweaks

---
+++ src/components/Takeaway/Takeaway.menu.js
@@ -0,0 +1,113 @@
+export const sideOrders = [
+  "Chips",
+  "Skin on French Fries",
+  "Garlic Fries",
+  "Salt & Chilli Fries",
+  "Kebab Fries",
+  "Tobacco Onions",
+  "Champ",
+  "Mash",
+]
+
+export const saucesAndSides = [
+  "Gravy",
+  "Pepper Sauce",
+  "Curry Sauce",
+  "Mayonaise",
+  "Garlic Mayonaise",
+  "BBQ Sauce",
+  "Kebab Sauce",
+  "Sweet Chilli Sauce",
+]
+
+export const treats = [
+  "Salted Caramel Brownie",
+  "Caramel Square",
+  "Mint Aero Square",
+  "Raspberry Ruffle",
+]
+
+export const breakfast = [
+  "Sausage",
+  "Bacon",
+  "Fried Egg",
+  "Poached Egg",
+  "Beans",
+  "Vegetable Roll",
+  "Black Pudding",
+  "White Pudding",
+  "Diced Potatoes",
+  "Soda Bread",
+  "Potato Bread",
+  "Pancake",
+]
+
+export const breakfastBap = "Choose from 2 fillings from Sausage, Bacon and Egg"
+
+export const porridge = [
+  "Fresh Fruit",
+  "Honey",
+  "Maple Syrup",
+  "Cinnamon",
+  "Nuts & Seeds",
+]
+
+export const takeaway = [
+  {
+    name: "Steak Burger",
+    description:
+      "Hand Pressed Burger, Bacon, Cheese, Gem Lettuce, Tomato, Red Onion, Secret Mayo, Toasted Bun",
+  },
+
+  {
+    name: "Chicken Burger",
+    description:
+      "Southern Fried Chicken Fillet, Bacon, Cheese, Gem Lettuce, Tomato, Red Onion, Mayo, Toasted Bun",
+  },
+  {
+    name: "Chicken Goujons",
+    description: "4 Cripsy Fried Chicken Fillets with a Sauce or Dip",
+  },
+  {
+    name: "Salt & Chilli Goujons",
+    description:
+      "4 Fried Goujons, seasoned with our Salt & Chilli Spice & served with Garlic Mayo",
+  },
+  {
+    name: "Sticky Chilli Chicken",
+    description: "Crispy Fried Chicken, Sweet Chilli Sauce, Garlic Mayo",
+  },
+  {
+    name: "Pan-Fried Chicken",
+    description: "Seasonal Vegetables, Tobacco Onions, Choice of Sauce",
+  },
+  {
+    name: "Fresh Cod",
+    description: "Crispy Battered Cod, Homemade Tartare & Lemon",
+  },
+  {
+    name: "Penne Pasta",
+    description: "Chilli Steak or Chicken Penne Pasta",
+  },
+]
+
+export const loadedChips = [
+  {
+    name: "Southern Fried Chicken",
+    description: "Tobacco Onions, Creamy Pepper Sauce",
+  },
+  {
+    name: "Spice Box",
+    description: "Salt & Chilli Chicken, Onions, Peppers, Garlic Mayo",
+  },
+  {
+    name: "Creamy Chilli",
+    description: "Steak or Chicken with Tobacco Onions",
+  },
+]
+
+export const kidsMeals = [
+  "Small Fish and Chips",
+  "2 Sausages and Chips",
+  "2 Chicken Goujons and Chips",
+]
