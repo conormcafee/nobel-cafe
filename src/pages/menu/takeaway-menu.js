39c7246663b6ba42910700420c1357695ea3afca
Conor McAfee <hello@conormcafee.co.uk>
2021-05-20 20:18:20 +0100

    Removed takeaway menu

--- src/pages/menu/takeaway-menu.js
+++ src/pages/menu/takeaway-menu.js
@@ -1,7 +1,7 @@
 import React from "react"
 import SEO from "../../components/seo"
 import { Layout } from "../../components/Layout"
-import Takeaway from "../../components/Takeaway/Takeaway"
+// import Takeaway from "../../components/Takeaway/Takeaway"
 
 const Desc = `Please call 028 2565 4302 to place an order.  Alternatively you can place your order at the cafe`
 
@@ -9,7 +9,7 @@ const TakeawayMenu = () => (
   <Layout>
     <SEO title="Takeaway Menu" description={Desc} />
 
-    <Takeaway />
+    {/* <Takeaway /> */}
   </Layout>
 )
 
