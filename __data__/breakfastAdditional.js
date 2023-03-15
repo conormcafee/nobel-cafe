067328f3a05665a9a2fa98d27175b1b4775464da
Conor McAfee <hello@conormcafee.co.uk>
2023-03-15 21:49:22 +0000

    Update breakfastAdditional.js

--- __data__/breakfastAdditional.js
+++ __data__/breakfastAdditional.js
@@ -1,25 +1,5 @@
 export const breakfastAdditional = [
   {
-    id: "reccOxxcvJVkNR7R8",
-    fields: {
-      name: "French Toast",
-      description: "Topped with sugar pit bacon, fried egg & maple syrup\n",
-      category: "breakfast-additional",
-      price: 6.55,
-    },
-    createdTime: "2021-11-16T17:09:36.000Z",
-  },
-  {
-    id: "rec0v8MDmW1bkU38H",
-    fields: {
-      name: "Toasted Sourdough",
-      description: "Topped with avocado, lean bacon & poached egg\n",
-      category: "breakfast-additional",
-      price: 6.55,
-    },
-    createdTime: "2021-11-16T17:09:36.000Z",
-  },
-  {
     id: "recQa3QfLJflIjugg",
     fields: {
       name: "Nobel Bap",
@@ -46,17 +26,6 @@ export const breakfastAdditional = [
       name: "Small Porridge",
       description:
         "Choose toppings from fresh fruit, honey, maple syrup, cinnamon, nuts & seeds\n",
-      category: "breakfast-additional",
-      price: 3.25,
-    },
-    createdTime: "2021-11-16T17:13:19.000Z",
-  },
-  {
-    id: "recJKwI1834gjaEVafR",
-    fields: {
-      name: "Gourmet Sausage Roll",
-      description:
-        "Pork & black pudding sausage roll, served with Ballymaloe country relish\n",
       category: "breakfast-additional",
       price: 3.25,
     },
