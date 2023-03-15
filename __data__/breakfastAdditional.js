a84bc309ed5c844d5a9ca53485c1aac63d926725
Conor McAfee <hello@conormcafee.co.uk>
2023-03-15 21:53:12 +0000

    Update breakfastAdditional.js

--- __data__/breakfastAdditional.js
+++ __data__/breakfastAdditional.js
@@ -5,7 +5,7 @@ export const breakfastAdditional = [
       name: "Nobel Bap",
       description: "Sausage, bacon & egg\n",
       category: "breakfast-additional",
-      price: 4.55,
+      price: 4.75,
     },
     createdTime: "2021-11-16T17:12:19.000Z",
   },
@@ -16,7 +16,7 @@ export const breakfastAdditional = [
       description:
         "Choose from Rhubarb compote & oat & almond crumble.  Seasonal berries, apple compote & cinamon.  Add honey or maple syrup or nuts & seeds.\n",
       category: "breakfast-additional",
-      price: 4.55,
+      price: 4.95,
     },
     createdTime: "2021-11-16T17:12:44.000Z",
   },
@@ -27,8 +27,52 @@ export const breakfastAdditional = [
       description:
         "Choose toppings from fresh fruit, honey, maple syrup, cinnamon, nuts & seeds\n",
       category: "breakfast-additional",
-      price: 3.25,
+      price: 3.75,
     },
     createdTime: "2021-11-16T17:13:19.000Z",
-  }
+  },
+  {
+    id: "recJKwIL2XAmEVafRa",
+    fields: {
+      name: "Spanish Eggs",
+      description:
+        "Toasted sourdough, avocado, fried eggs & chorizo and red pepper",
+      category: "breakfast-additional",
+      price: 7.75,
+    },
+    createdTime: "2021-11-16T17:13:19.000Z",
+  },
+  {
+    id: "recJKwIL2XAmEVafRb",
+    fields: {
+      name: "Bombay Benny",
+      description:
+        "Toasted muffins, creamed spinach, smoked salmon, poached eggs & curried hollandaise",
+      category: "breakfast-additional",
+      price: 7.75,
+    },
+    createdTime: "2021-11-16T17:13:19.000Z",
+  },
+  {
+    id: "recJKwIL2XAmEVafRc",
+    fields: {
+      name: "Bananahama",
+      description:
+        "French toast, banana fritter, sugar pit bacon & maple syrup",
+      category: "breakfast-additional",
+      price: 7.75,
+    },
+    createdTime: "2021-11-16T17:13:19.000Z",
+  },
+  {
+    id: "recJKwIL2XAmEVafRd",
+    fields: {
+      name: "Lean on Green",
+      description:
+        "Toasted sourdough, avocado, lean back bacon & poached eggs ",
+      category: "breakfast-additional",
+      price: 7.75,
+    },
+    createdTime: "2021-11-16T17:13:19.000Z",
+  },
 ]
