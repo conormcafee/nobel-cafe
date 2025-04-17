147a065c526378be2afcfe9d4ed1df9e401a6b6c
Conor McAfee <conor@white.space>
2025-04-17 15:55:24 +0100

    Added Spring 2025 menu

--- pages/menu.js
+++ pages/menu.js
@@ -1,15 +1,29 @@
 import { Layout } from "@components/layout";
-import { Breakfast, Lunch, Sandwiches } from "@components/menu";
-
-import { data } from "@data/menu";
-
-const { records = [] } = data;
 
 const meta = {
   title: "Breakfast & Lunch Menu",
   description:
     "Delicious breakfast & lunch serverd Monday - Saturday in Nobel Cafe, Ballymena",
 };
+
+const images = [
+  {
+    src: "/images/menu/april-2025/breakfast-front.jpeg",
+    alt: "Breakfast Menu (Front)",
+  },
+  {
+    src: "/images/menu/april-2025/breakfast-back.jpeg",
+    alt: "Breakfast Menu (Back)",
+  },
+  {
+    src: "/images/menu/april-2025/lunch-front.jpeg",
+    alt: "Lunch Menu (Front)",
+  },
+  {
+    src: "/images/menu/april-2025/lunch-back.jpeg",
+    alt: "Lunch Menu (Back)",
+  },
+]
 
 export default function Menu() {
   return (
@@ -22,9 +36,25 @@ export default function Menu() {
           </span>
         </h1>
       </div>
-      <Breakfast records={records} />
-      <Sandwiches records={records} />
-      <Lunch records={records} />
+
+
+      <div className="wrapper py-16 md:py-24 flex flex-wrap">
+
+        {images.map((image, index) => (
+          <div key={index} className="w-full md:w-1/2 lg:w-1/4 p-4 overflow-hidden">
+            <a href={image.src} target="_blank" rel="noopener noreferrer" className="block pointer border-2 border-green-500 rounded-lg shadow-md overflow-hidden hover:transform hover:scale-105 transition duration-300">
+              <img
+                className="w-full h-[200px] object-top object-cover"
+                src={image.src}
+                alt={image.alt}
+              />
+            </a>
+            <p className="mt-4">{image.alt}</p>
+          </div>
+        ))}
+
+
+      </div>
     </Layout>
   );
 }
